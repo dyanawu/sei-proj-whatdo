d2c436392c75d06d42b66862d2f90dc929cba659
Dyana Wu <dyanawu@gmail.com>
2020-06-09 14:17:16 +0800

    Add landing page conditional render and change title

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,6 +1,7 @@
 class LandingController < ApplicationController
   def index
     if current_user
+      # render the onepage thing here
       render plain: "#{current_user.username} logged in"
     end
   end
