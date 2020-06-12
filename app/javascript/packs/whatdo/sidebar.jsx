120dd5bc5d228ad78f467947afef483a1241dace
Dyana Wu <dyanawu@gmail.com>
2020-06-12 14:05:52 +0800

    Add additional items to sidebar

--- app/javascript/packs/whatdo/sidebar.jsx
+++ app/javascript/packs/whatdo/sidebar.jsx
@@ -2,26 +2,28 @@ import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 
 export default class Sidebar extends Component {
+  clickHandler(e) {
+    this.props.fetchListsByTag(e.target.id);
+  }
+
   render() {
     const tags = this.props.tags;
     const tagList = tags.map(t => {
       return (
-        <>
-          <a
-            key={t.id + t.name}
-            id={t.id}
-            href="#"
-            className="nav-item nav-link"
-            onClick={(e) => this.props.fetchListsByTag(e.target.id)}
-          >
-            {t.name}
-          </a>
-        </>
+        <a
+          key={t.id + t.name}
+          id={t.id}
+          href="#"
+          className="nav-item nav-link"
+          onClick={(e) => this.clickHandler(e)}
+        >
+          {t.name}
+        </a>
       )
     });
     return (
       <>
-        <h4>tags</h4>
+        <h4>
           <a
             href="#"
             className="nav-item nav-link"
@@ -29,9 +31,36 @@ export default class Sidebar extends Component {
           >
             all lists
           </a>
+        </h4>
+        <h4>
+          <a
+            href="#"
+            className="nav-item nav-link disabled"
+          >
+            tags
+          </a>
+        </h4>
         <nav className="nav flex-column">
           {tagList}
         </nav>
+        <h4>
+          <a
+            href="#"
+            className="nav-item nav-link"
+            onClick={(e) => this.props.fetchUntaggedLists()}
+          >
+            untagged lists
+          </a>
+        </h4>
+        <h4>
+          <a
+            href="#"
+            className="nav-item nav-link"
+            onClick={(e) => console.log(e)}
+          >
+            TODO: edit tags
+          </a>
+        </h4>
       </>
     );
   }
