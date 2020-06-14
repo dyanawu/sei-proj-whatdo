09aaea0db088835425f88cb57ff08596424067cd
Dyana Wu <dyanawu@gmail.com>
2020-06-14 15:18:12 +0800

    Add elements to list form

--- app/javascript/packs/whatdo/components/listform.jsx
+++ app/javascript/packs/whatdo/components/listform.jsx
@@ -2,14 +2,50 @@ import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 
 export default class ListForm extends Component {
+  constructor() {
+    super();
+  }
+
+  addItemToList(item, listId) {
+    console.log("add to", listId);
+    console.log(item);
+  }
+
+  setItem(e) {
+    this.setState({newItemName: e.target.value});
+    console.log(this.state.newItemName);
+  }
+
   render() {
+    const currentList = this.state.currentList;
     return (
-      <button
-        onClick={() => this.props.delList(this.props.currentList)}
-        className="btn btn-dark"
-      >
-        delete list
-      </button>
+      <>
+        <div className="col-md-8 no-gutters p-0">
+          <input
+            className="form-control"
+            placeholder="new list item"
+            defaultValue={this.props.newItemName}
+            onChange={(e) => this.setItem(e)}
+          />
+        </div>
+        <div className="col-md-2 no-gutters px-auto py-0">
+          <button
+            onClick={() => this.addItemToList(this.state.newItemName, currentList)}
+            className="btn btn-dark"
+          >
+            add item
+          </button>
+        </div>
+
+        <div className="col-md-2 no-gutters px-auto py-0">
+          <button
+            onClick={() => this.props.delList(currentList)}
+            className="btn btn-dark"
+          >
+            delete list
+          </button>
+        </div>
+      </>
     );
   }
 }
